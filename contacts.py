fdb29508a518436168988d7dba86edb363d496f7
Lisa <lisa.das9374@gmail.com>
2022-09-02 16:14:40 +0530

    [Lisa] Refactor:Added new class to access data

--- contacts.py
+++ contacts.py
@@ -1,68 +1,28 @@
 """
     @Author: Lisa Das
     @Date: 2022-08-31
-    @Last Modified time: 2022-09-01
+    @Last Modified time: 2022-09-02
     @Title : Create an Address Book
 """
 
 class Person:
-    contact_list = []
-    def __init__(self, first, last, address, city, state, zip, phone_no, email):
+    def __init__(self, first, last, address, city, state, zip_code, phone_no, email):
         self.first = first
         self.last = last
         self.address = address
         self.city = city
         self.state = state
-        self.zip = zip
+        self.zip_code = zip_code
         self.phone_no = phone_no
         self.email = email
 
-    def addNewPerson(self):
-        """
-        Description:
-            Adding new contact to the contact list
-        """
-        Person.contact_list.append(self)
-    def getPersonList(self):
-        """
-        Description:
-            Printing new contact list
-        Return:
-            List of contact
-        """
-        return Person.contact_list
-    def setFirst(self,first):
-        self.first = first
-    def getFirst(self):
-        return self.first
-    def setLast(self,last):
-        self.last = last
-    def getLast(self):
-        return self.last
-    def setAddress(self,address):
-        self.address = address
-    def getAddress(self):
-        return self.address
-    def setCity(self,city):
-        self.city = city
-    def getCity(self):
-        return self.city
-    def setState(self,state):
-        self.city = state
-    def getState(self):
-        return self.state
-    def setZip(self,zip):
-        self.zip = zip
-    def getZip(self):
-        return self.zip
-    def setPhone_no(self,phone_no):
-        self.phone_no = phone_no
-    def getPhone_no(self):
-        return self.phone_no
-    def setEmail(self,email):
-        self.email = email
-    def getEmail(self):
-        return self.email
+    @property
+    def get(self):
+        return {'first': self.first, 'last': self.last, 'address': self.address, 'city': self.city, 'state': self.state, 'zip_code': self.zip_code, 'phone_no': self.phone_no, 'email': self.email}
 
-    def __str__(self):
-        return "%s %s %s %s %s %d %d %s"%(self.first, self.last, self.address, self.city, self.state, self.zip, self.phone_no, self.email)
+class AddressBook:
+    def __init__(self):
+        self.contacts = []
+
+    def add_contact(self, contact):
+        self.contacts.append(contact)
--- main.py
+++ main.py
@@ -1,34 +1,25 @@
 """
     @Author: Lisa Das
     @Date: 2022-08-31
-    @Last Modified time: 2022-09-01
+    @Last Modified time: 2022-09-02
     @Title : Create an Address Book
 """
 
 # Importing contacts module
-import contacts
+from contacts import Person, AddressBook
 
 # Driver code
 if __name__ == '__main__':
     print("Welcome to the address book program")
-    choice = 1
-    person = contacts.Person("", "", "", "", "", 0, 0, "")
-    while choice >= 1 and choice <= 2:
-        print("\n1. Add New Person contact\n2. Get All Person Contact List\n")
-        choice = int(input("Enter Your Choice or press 0 to exit: "))
-        if(choice == 1):
-            first = input("Enter your first name: ")
-            last = input("Enter your last name: ")
-            address = input("Enter your address: ")
-            city = input("Enter your city: ")
-            state = input("Enter your state: ")
-            zip = int(input("Enter zip code: "))
-            phone_no = int(input("Enter your contact number: "))
-            email = input("Enter your email address: ")
-            list_person = contacts.Person(first, last, address, city, state, zip, phone_no, email)
-            list_person.addNewPerson()
-
-        elif(choice == 2):
-            print("\n")
-            for list_person in person.getPersonList():
-                print(list_person)
+    first = input("Enter your first name: ")
+    last = input("Enter your last name: ")
+    address = input("Enter your address: ")
+    city = input("Enter your city: ")
+    state = input("Enter your state: ")
+    zip_code = int(input("Enter zip code: "))
+    phone_no = int(input("Enter your contact number: "))
+    email = input("Enter your email address: ")
+    list_person = Person(first, last, address, city, state, zip_code, phone_no, email)
+    address_book = AddressBook()
+    print(list_person.get)
+    address_book.add_contact(list_person.get)
