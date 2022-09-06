2fc1b99e05bdcc928e5a2d5c0223df11aa7ec46a
Lisa <lisa.das9374@gmail.com>
2022-09-06 18:39:18 +0530

    [Lisa] Add: Searching contact as per city list

--- contacts.py
+++ contacts.py
@@ -1,24 +1,32 @@
 """
     @Author: Lisa Das
     @Date: 2022-08-02
-    @Last Modified date: 2022-09-05
-    @Title : Create an Address Book
+    @Last Modified date: 2022-09-06
+    @Title : Create an Address Book and modifying with conditions
 """
 
 class Person:
-    def __init__(self, first, last, address, city, state, zip_code, phone_no, email):
-        self.first = first
-        self.last = last
+    def __init__(self, first_name, last_name, address, city, state, zip_code, phone_no, email_id):
+        self.first_name = first_name
+        self.last_name = last_name
         self.address = address
         self.city = city
         self.state = state
         self.zip_code = zip_code
         self.phone_no = phone_no
-        self.email = email
+        self.email_id = email_id
 
     @property
     def get(self):
-        return {'first': self.first, 'last': self.last, 'address': self.address, 'city': self.city, 'state': self.state, 'zip_code': self.zip_code, 'phone_no': self.phone_no, 'email': self.email}
+        return {'first_name': self.first_name,
+                'last_name': self.last_name, 
+                'address': self.address, 
+                'city': self.city, 
+                'state': self.state, 
+                'zip_code': self.zip_code, 
+                'phone_no': self.phone_no, 
+                'email_id': self.email_id
+                }
         
 class AddressBook:
     def __init__(self):
@@ -28,7 +36,7 @@ class AddressBook:
     def get(self):
         return self.contacts
 
-    def add_contact(self, first, contact):
+    def add_contact(self, first_name, contact):
         """
         Description:
             Adding new contact to the list
@@ -40,7 +48,7 @@ class AddressBook:
         
         self.contacts.append(contact)
 
-    def check_duplicate(self, first):
+    def check_duplicate(self, first_name):
         """
         Description:
             Checking for contact duplicacy
@@ -50,11 +58,11 @@ class AddressBook:
             Return True if first name is already present in the list else false
         """
         for contact in self.contacts:
-            if contact["first"] == first:
+            if contact["first_name"] == first_name:
                 return True
         return False
     
-    def edit_contact(self, first, new_name):
+    def edit_contact(self, first_name, new_name):
         """
         Description:
             Editing existing contact to the list
@@ -64,17 +72,17 @@ class AddressBook:
             List of contact in the form of dictionery
         """
         for contact in self.contacts:
-            if contact["first"] == first:
-                contact["first"] = new_name["first"]
-                contact["last"] = new_name["last"]
+            if contact["first_name"] == first_name:
+                contact["first_name"] = new_name["first_name"]
+                contact["last_name"] = new_name["last_name"]
                 contact["address"] = new_name["address"]
                 contact["city"] = new_name["city"]
                 contact["state"] = new_name["state"]
                 contact["zip_code"] = new_name["zip_code"]
                 contact["phone_no"] = new_name["phone_no"]
-                contact["email"] = new_name["email"]
+                contact["email_id"] = new_name["email_id"]
 
-    def delete_contact(self, first):
+    def delete_contact(self, first_name):
         """
         Description:
             Deleting existing contact to the list
@@ -82,7 +90,7 @@ class AddressBook:
             First name to search for details
         """
         for i, contact in enumerate(self.contacts):
-            if contact["first"] == first:
+            if contact["first_name"] == first_name:
                 del self.contacts[i]
 
     def search_contact_by_city(self, searching_city):
@@ -114,6 +122,34 @@ class AddressBook:
             if contact["state"] == searching_state:
                 list_of_contact_in_the_city.append(contact)
         return list_of_contact_in_the_city
+
+    def view_city(self):
+        """
+        Description:
+            Fetching all the city name
+        Parameter:
+            None
+        Returns:
+            Set of city name 
+        """
+        set_of_city = set()
+        for contact in self.contacts:
+            set_of_city.add(contact["city"])
+        return set_of_city
+
+    def view_state(self):
+        """
+        Description:
+            Fetching all the state name
+        Parameter:
+            None
+        Returns:
+            Set of state name 
+        """
+        set_of_state = set()
+        for contact in self.contacts:
+            set_of_state.add(contact["state"])
+        return set_of_state
 
     def display_contact(self):
         """
--- main.py
+++ main.py
@@ -1,41 +1,50 @@
 """
     @Author: Lisa Das
     @Date: 2022-09-05
-    @Last Modified date: 2022-09-05
-    @Title : Create an Address Book
+    @Last Modified date: 2022-09-06
+    @Title : Create an Address Book and modifying with conditions
 """
 
 # Importing contacts module
 from contacts import Person, AddressBook
 from addressbook_collection import MulAddressBook
 
-
-# Driver code
-if __name__ == '__main__':
+def main():
     print("Welcome to the address book program")
     choice = 1
     address_book = AddressBook()
     address_book_collection = MulAddressBook()
     while choice >= 1 and choice <= 9:
-        print("\n1. Add New Person contact\n2. Switch to the Address Book \n3. Edit existing contact details\n4. Delete existing contact details\n5. Searching contacts as per city \n6. Searching contacts as per state\n7. Display contact details \n8. Display Full address book contact details\n")
+        print("""
+        1. Add New Person contact
+        2. Switch to the Address Book
+        3. Edit existing contact details
+        4. Delete existing contact details
+        5. Searching contacts as per city
+        6. View contact details as per city
+        7. View contact details as per state
+        8. Searching contacts as per state
+        9. Display contact details
+        10. Display Full address book contact details\n""")
+
         choice = int(input("Enter Your Choice: "))
 
         if(choice == 1):
             while True:
-                first = input("Enter your first name: ")
-                if not address_book.check_duplicate(first):
+                first_name = input("Enter your first name: ")
+                if not address_book.check_duplicate(first_name):
                     break
                 print("Enter new contact details")
-            last = input("Enter your last name: ")
+            last_name = input("Enter your last name: ")
             address = input("Enter your address: ")
             city = input("Enter your city: ")
             state = input("Enter your state: ")
             zip_code = input("Enter zip code: ")
             phone_no = input("Enter your contact number: ")
-            email = input("Enter your email address: ")
-            list_person = Person(first, last, address, city, state, zip_code, phone_no, email)
+            email_id = input("Enter your email address: ")
+            list_person = Person(first_name, last_name, address, city, state, zip_code, phone_no, email_id)
             print(list_person.get)
-            address_book.add_contact(first, list_person.get)
+            address_book.add_contact(first_name, list_person.get)
 
         elif(choice == 2):
             address_book_name = input("Enter the name of the address book: ")
@@ -43,15 +52,15 @@ if __name__ == '__main__':
 
         elif(choice == 3):
             first_name = input("Enter the first name which you want to change: ")
-            first = input("Enter your first name: ")
-            last = input("Enter your last name: ")
+            first_name = input("Enter your first name: ")
+            last_name = input("Enter your last name: ")
             address = input("Enter your address: ")
             city = input("Enter your city: ")
             state = input("Enter your state: ")
             zip_code = input("Enter zip code: ")
             phone_no = input("Enter your contact number: ")
-            email = input("Enter your email address: ")
-            edited_list_person = Person(first, last, address, city, state, zip_code, phone_no, email)
+            email_id = input("Enter your email address: ")
+            edited_list_person = Person(first_name, last_name, address, city, state, zip_code, phone_no, email_id)
             print(edited_list_person.get)
             address_book.edit_contact(first_name, edited_list_person.get) 
 
@@ -64,15 +73,38 @@ if __name__ == '__main__':
             city_name = input("Enter name of the city ")
             searched_contact = address_book.search_contact_by_city(city_name)
             print(searched_contact)
-
+            
         elif(choice == 6):
+            list_of_city = address_book.view_city()
+            for city in list_of_city:
+                print("Name of the city: ", city)
+                contact_list = address_book.search_contact_by_city(city)
+                for contact in contact_list:
+                    for key in contact:
+                        print(key, ' : ', contact[key])
+
+        elif(choice == 7):
+            list_of_state = address_book.view_state()
+            for state in list_of_state:
+                print("Name of the city: ", state)
+                contact_list = address_book.search_contact_state(state)
+                for contact in contact_list:
+                    for key in contact:
+                        print(key, ' : ', contact[key])
+                
+        elif(choice == 8):
             state_name = input("Enter the name of the state ")
             searched_contact = address_book.search_contact_state(state_name)
             print(searched_contact)
 
-        elif(choice == 7):
+        elif(choice == 9):
             address_book.display_contact()
 
-        elif(choice == 8):
+        elif(choice == 10):
             address_book_collection.show_contacts()
             print("All Address Book displayed")
+
+
+# Driver code
+if __name__ == '__main__':
+    main()
